4b3fa403aa556aef83171570abbf5318a2828e54
Semyon Okulov <67464545+scffs@users.noreply.github.com>
2025-06-28 21:48:55 +0700

    chore: move log

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,6 @@ import { tariffSyncService } from "#jobs/tariffSync/tariffSync.js";
 await migrate.latest();
 await seed.run();
 
-tariffSyncService.start();
-
 console.log("All migrations and seeds have been run");
+
+tariffSyncService.start();
